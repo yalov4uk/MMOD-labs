58914eb01ba4be30ad5ab3f2f81f672550529415
yalov4uk <valera.yalow4uk@gmail.com>
2017-10-17 15:41:34 +0300

    refactor lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,95 +1,83 @@
-import numpy as np
-from matplotlib import pyplot, mlab
 import math
+
 import scipy.stats as st
+from matplotlib import pyplot, mlab
+
+from lab1.lab1 import method_2_
 
 
-def MKM(count, n=30, k=123456789, A0=2):
-    """
-    Function generating a number by a multiplicative congruential method.
-    """
-    m = 2 ** n
-    A = list()
-    A.append(A0)
-    for i in xrange(count):
-        A.append((k * A[i]) % m)
+class Lab3:
+    def __init__(self):
+        self.l = 1.5
+        self.x_min = 0.0
+        self.x_max = 5.0
+        self.n = 100
+        self.h = (self.x_max - self.x_min) / self.n
+        self.x = [i * self.h for i in xrange(self.n)]
+        self.y = [self.l * (math.e ** (-self.l * self.x[i])) for i in xrange(len(self.x))]
+        self.F = [1 - math.e ** (-self.l * self.x[i]) for i in xrange(len(self.x))]
 
-    z = [A[i] / float(m) for i in xrange(1, len(A))]
-    return z
+    def run(self):
+        pyplot.figure(figsize=(15, 5))
+        y1 = self.generate_contineous_law(1, 100)
+        y2 = self.generate_contineous_law(2, 10000)
 
+        pyplot.figure(figsize=(15, 5))
+        F1 = self.generate_function_distribution(1, y1)
+        F2 = self.generate_function_distribution(2, y2)
 
-"""
-Generate a continuous random variable distributed according to the exponential law by 
-the inverse function method and construct the distribution histograms and the theoretical 
-distribution density. Compare the results.
-"""
-l = 2
-pyplot.figure(figsize=(16, 6))
-x_min = 0.0
-x_max = 6.0
-n = 100
-h = (x_max - x_min) / n
-x = [i * h for i in xrange(n)]
-lmbd = 1
-y = [lmbd * (math.e ** (-lmbd * x[i])) for i in xrange(len(x))]
+        self.kolmogorov(y1, y2, F1, F2)
 
-pyplot.subplot(1, 2, 1)
-x1 = MKM(100)
-y1 = [-1.0 * (1.0 / l) * math.log(1 - x1[i]) for i in xrange(len(x1))]
-pyplot.hist(y1, normed=1)
-M1 = sum(y1) / len(y1)
-D1 = sum([y1[i] ** 2 - M1 ** 2 for i in xrange(len(y1))]) / len(y1)
-pyplot.title('{0} numbers. M = {1}. D = {2}'.format(len(y1), M1, D1))
-pyplot.plot(x, y)
+        pyplot.figure(figsize=(5, 5))
+        self.confidence(y1)
 
-pyplot.subplot(1, 2, 2)
-x2 = MKM(10000)
-y2 = [-1.0 * (1.0 / l) * math.log(1 - x2[i]) for i in xrange(len(x2))]
-pyplot.hist(y2, normed=1)
-M2 = sum(y2) / len(y2)
-D2 = sum([y2[i] ** 2 - M2 ** 2 for i in xrange(len(y2))]) / len(y2)
-pyplot.title('{0} numbers. M = {1}. D = {2}'.format(len(y2), M2, D2))
-pyplot.plot(x, y)
+        pyplot.show()
 
+    def generate_contineous_law(self, i, n):
+        pyplot.subplot(1, 2, i)
+        gen = method_2_()
+        xn = [next(gen) for _ in xrange(n)]
+        yn = [-1.0 * (1.0 / self.l) * math.log(1 - xn[i]) for i in xrange(len(xn))]
+        pyplot.hist(yn, normed=1)
+        M = self.calculate_M(yn)
+        D = self.calculete_D(yn, M)
+        pyplot.title('n = {},  M = {}. D = {}'.format(len(yn), M, D))
+        pyplot.plot(self.x, self.y)
+        return yn
 
-"""
-Let's construct a theoretical distribution function and empirical distribution functions for sampling volumes of 
-100 and 10000, respectively. Compare the results.
-"""
-pyplot.figure(figsize=(16, 6))
-F = [1 - math.e ** (-l * x[i]) for i in xrange(len(x))]
+    def generate_function_distribution(self, i, y):
+        pyplot.subplot(1, 2, i)
+        y.sort()
+        F = [i / float(len(y)) for i in xrange(len(y))]
+        pyplot.title("Exponential function {}".format(len(y)))
+        pyplot.plot(y, F)
+        pyplot.plot(self.x, self.F)
+        return F
 
-pyplot.subplot(1, 2, 1)
-y1.sort()
-F1 = [i / float(len(y1)) for i in xrange(len(y1))]
-pyplot.title("theoretical exponential function with sampling : {}".format(len(y1)))
-pyplot.plot(y1, F1)
-pyplot.plot(x, F)
+    def kolmogorov(self, y1, y2, F1, F2):
+        F1_teor = [1 - math.e ** (-self.l * y1[i]) for i in xrange(len(y1))]
+        F2_teor = [1 - math.e ** (-self.l * y2[i]) for i in xrange(len(y2))]
+        print math.sqrt(self.n) * max(abs(F1[i] - F1_teor[i]) for i in xrange(len(y1)))
+        print math.sqrt(self.n) * max(abs(F2[i] - F2_teor[i]) for i in xrange(len(y2)))
 
-pyplot.subplot(1, 2, 2)
-y2.sort()
-F2 = [i / float(len(y2)) for i in xrange(len(y2))]
-pyplot.title("empirical exponential function with sampling : {}".format(len(y2)))
-pyplot.plot(y2, F2)
-pyplot.plot(x, F)
+    def confidence(self, y):
+        x_min = 0.01
+        x_max = 0.99
+        dx = 0.01
+        xlist = mlab.frange(x_min, x_max, dx)
+        ylist = [
+            st.norm.ppf((i + 1) / 2) * 2 * math.sqrt(self.calculete_D(y, self.calculate_M(y))) / math.sqrt(self.n - 1)
+            for i in xlist]
+        pyplot.plot(xlist, ylist, 'b')
 
+    def calculate_M(self, y):
+        """
+        M = 1 / l
+        """
+        return sum(y) / len(y)
 
-"""
-Check the Kolmogorov test of agreement.
-"""
-F1_teor = [1 - math.e ** (-l * y1[i]) for i in xrange(len(y1))]
-F2_teor = [1 - math.e ** (-l * y2[i]) for i in xrange(len(y2))]
-print math.sqrt(n) * max(abs(F2[i] - F2_teor[i]) for i in xrange(len(y2)))
-
-"""
-Let's construct a graph of the dependence of the value of the confidence interval, on the value 
-of the confidence probability for the mathematical expectation.
-"""
-x_min = 0.01
-x_max = 0.99
-dx = 0.01
-xlist = mlab.frange(x_min, x_max, dx)
-ylist = [st.norm.ppf((i + 1) / 2) * 2 * math.sqrt(D1) / math.sqrt(n - 1) for i in xlist]
-pyplot.plot(xlist, ylist, 'b')
-
-pyplot.show()
+    def calculete_D(self, y, M):
+        """
+        D = 1 / l ^ 2
+        """
+        return sum([y[i] ** 2 - M ** 2 for i in xrange(len(y))]) / len(y)
--- lab3/main.py
+++ lab3/main.py
@@ -1,3 +1,5 @@
 from lab3 import *
 
 if __name__ == '__main__':
+    lab3 = Lab3()
+    lab3.run()
