8b2b37f83ddbf34a4cb3af5b38b0bdfc6eb8f3c3
yalov4uk <valera.yalow4uk@gmail.com>
2017-10-17 16:35:47 +0300

    refactor lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -14,8 +14,8 @@ class Lab3:
         self.n = 100
         self.h = (self.x_max - self.x_min) / self.n
         self.x = [i * self.h for i in xrange(self.n)]
-        self.y = [self.l * (math.e ** (-self.l * self.x[i])) for i in xrange(len(self.x))]
-        self.F = [1 - math.e ** (-self.l * self.x[i]) for i in xrange(len(self.x))]
+        self.y = [self.l * (math.e ** (-self.l * self.x[i])) for i in xrange(self.n)]
+        self.F = [1 - math.e ** (-self.l * self.x[i]) for i in xrange(self.n)]
 
     def run(self):
         pyplot.figure(figsize=(15, 5))
@@ -26,10 +26,11 @@ class Lab3:
         F1 = self.generate_function_distribution(1, y1)
         F2 = self.generate_function_distribution(2, y2)
 
-        self.kolmogorov(y1, y2, F1, F2)
+        k2 = self.kolmogorov(y2, F2)
+        print k2
 
-        pyplot.figure(figsize=(5, 5))
-        self.confidence(y1)
+        pyplot.figure(figsize=(15, 5))
+        self.confidence(1, y1)
 
         pyplot.show()
 
@@ -37,11 +38,11 @@ class Lab3:
         pyplot.subplot(1, 2, i)
         gen = method_2_()
         xn = [next(gen) for _ in xrange(n)]
-        yn = [-1.0 * (1.0 / self.l) * math.log(1 - xn[i]) for i in xrange(len(xn))]
+        yn = [-1.0 * (1.0 / self.l) * math.log(1 - xn[i]) for i in xrange(n)]
         pyplot.hist(yn, normed=1)
         M = self.calculate_M(yn)
         D = self.calculete_D(yn, M)
-        pyplot.title('n = {},  M = {}. D = {}'.format(len(yn), M, D))
+        pyplot.title('n = {},  M = {}. D = {}'.format(n, M, D))
         pyplot.plot(self.x, self.y)
         return yn
 
@@ -54,13 +55,12 @@ class Lab3:
         pyplot.plot(self.x, self.F)
         return F
 
-    def kolmogorov(self, y1, y2, F1, F2):
-        F1_teor = [1 - math.e ** (-self.l * y1[i]) for i in xrange(len(y1))]
-        F2_teor = [1 - math.e ** (-self.l * y2[i]) for i in xrange(len(y2))]
-        print math.sqrt(self.n) * max(abs(F1[i] - F1_teor[i]) for i in xrange(len(y1)))
-        print math.sqrt(self.n) * max(abs(F2[i] - F2_teor[i]) for i in xrange(len(y2)))
+    def kolmogorov(self, y, F):
+        F_teor = [1 - math.e ** (-self.l * y[i]) for i in xrange(len(y))]
+        return math.sqrt(self.n) * max(abs(F[i] - F_teor[i]) for i in xrange(len(y)))
 
-    def confidence(self, y):
+    def confidence(self, i, y):
+        pyplot.subplot(1, 2, i)
         x_min = 0.01
         x_max = 0.99
         dx = 0.01
@@ -71,13 +71,9 @@ class Lab3:
         pyplot.plot(xlist, ylist, 'b')
 
     def calculate_M(self, y):
-        """
-        M = 1 / l
-        """
+        """ M = 1/lambda """
         return sum(y) / len(y)
 
     def calculete_D(self, y, M):
-        """
-        D = 1 / l ^ 2
-        """
+        """ D = 1/lambda^2 """
         return sum([y[i] ** 2 - M ** 2 for i in xrange(len(y))]) / len(y)
